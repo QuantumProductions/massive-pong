235d506299e4521feed6fb7c4f9a7d21ec701f3b
quantumpotato <quantumpotato@gmail.com>
2016-11-09 00:42:07 -0500

    Strict mode

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class Constants {
 	static constants() {
 		return {"ball_r" : Math.floor(Math.random() * 7) + 3, "paddle_r" : Math.floor(Math.random() * 30) + 30, 'maxShips' : 6, "paddle_w" : Math.floor(Math.random() * 10) + 5};
