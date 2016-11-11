c93f59b1cbedd6b0ea5cb6f3c66ec6a1df04cf41
quantumpotato <quantumpotato@gmail.com>
2016-11-11 18:04:01 -0500

    Placement

--- utils.js
+++ utils.js
@@ -2,15 +2,26 @@
 
 class Utils {
 	static shipPlacement(ships, fw, fh, max, offset) {
-		let spacing = fw / max;
-		var x = offset > 0 ? offset : 0;
-		let xOffset = 0.5 * spacing;
-		for (var s of ships) {
-			if (s.x == x + xOffset) {
-				return Utils.shipPlacement(ships, fw, fh, max, x + spacing);
-			}
+		let mod = ships.length % 2;
+		var offset = 0;
+		if (mod == 0) {
+
+		} else {
+			offset = fw / 2;
 		}
-		return x + xOffset;
+
+		var x = Math.floor(Math.random() * (fw / 2));
+		x += offset;
+		return x;
+		// let spacing = fw / max;
+		// var x = offset > 0 ? offset : 0;
+		// let xOffset = 0.5 * spacing;
+		// for (var s of ships) {
+		// 	if (s.x == x + xOffset) {
+		// 		return Utils.shipPlacement(ships, fw, fh, max, x + spacing);
+		// 	}
+		// }
+		// return x + xOffset;
 	}
 }
 
